e10f5c291273eba0604a34568ad5db1a8f031b3e
Wender Barbosa <wenblack012@gmail.com>
2023-07-03 11:58:53 -0300

    ✅ Altered homePage  ✅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,23 @@
 import ReactMarkdown from 'react-markdown'
 
-const markdown = `A paragraph with *emphasis* and **strong importance**.
 
-> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
-
-* Lists
-* [ ] todo
-* [x] done
-
-A table:
-
-
-`
 
 
 export default function Home (){
     return(
     <main className='flex  gap-4 flex-col w-screen items-center justify-center'>    
         <h1>API</h1>
-        <ReactMarkdown  children={markdown} />
-    </main>
+        <ReactMarkdown>
+            ``A paragraph with *emphasis* and **strong importance**.
 
+ A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+* [ ] todo
+* [x] done
+
+A table:`
+        </ReactMarkdown>
+    </main>
     )
 }
